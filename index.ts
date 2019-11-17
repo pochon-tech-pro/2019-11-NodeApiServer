485df08c1009fa3fa163a178ba69557434aa8e90
pochon-tech <pochon.tech.0519@gmail.com>
2019-11-17 18:34:10 +0900

    fix

--- index.ts
+++ index.ts
@@ -2,17 +2,24 @@ import express from 'express'
 
 const app: express.Express = express()
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const router: express.Router = express.Router()
 
-router.get('/api/get', (req:express.Request, res:express.Response) => {
+router.get('/api/getTest', (req:express.Request, res:express.Response) => {
   res.send(req.query) // curl localhost:3000/api/get?name=hoge
 })
-router.post('/api/post', (req:express.Request, res:express.Response) => {
+router.post('/api/postTest', (req:express.Request, res:express.Response) => {
   res.send(req.body) // curl -X POST localhost:3000/api/post -d "name=hoge"
 })
 app.use(router)
 
 app.listen(3000,()=>{ console.log('Example app listening on port 3000!') })
+
